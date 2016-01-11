5451ac1bc275c1400af9e6fa28e7cd18f190c9b8
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-01-11 05:08:35 +0200

    Duct abstractions
    * Demo of two endpoints merged into the handler, plus middleware common
      to both

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,3 +1,8 @@
+-- name: create-tables!
+-- create the tables if they don't exist
+CREATE TABLE IF NOT EXISTS DIRECTORS(ID IDENTITY PRIMARY KEY, NAME VARCHAR(255));
+CREATE TABLE IF NOT EXISTS AUTHORS(ID IDENTITY PRIMARY KEY, NAME VARCHAR(255));
+
 -- name: directors
 -- Return all directors.
 SELECT *
@@ -12,10 +17,24 @@ FROM DIRECTORS
 -- Insert a director
 INSERT INTO DIRECTORS ( name ) VALUES ( :name )
 
--- name: create-table!
--- create the directors table if it does not exist
-CREATE TABLE IF NOT EXISTS DIRECTORS(ID IDENTITY PRIMARY KEY, NAME VARCHAR(255))
-
 -- name: delete-director!
 -- Deletes a director
 DELETE FROM DIRECTORS WHERE NAME=:name
+
+-- name: authors
+-- Return all authors.
+SELECT *
+FROM AUTHORS
+
+-- name: count-authors
+-- Count all the authors
+SELECT count(*) AS count
+FROM AUTHORS
+
+-- name: save-author<!
+-- Insert a author
+INSERT INTO AUTHORS ( name ) VALUES ( :name )
+
+-- name: delete-author!
+-- Deletes an author
+DELETE FROM AUTHORS WHERE NAME=:name
--- src/example/systems.clj
+++ src/example/systems.clj
@@ -5,28 +5,52 @@
    (system.components 
     [h2 :refer [new-h2-database DEFAULT-MEM-SPEC DEFAULT-DB-SPEC]]
     [http-kit :refer [new-web-server]]
-    [app :refer [new-app]])
-   [example.handler :refer [app]]
-   [example.db :refer [create-table!]]
+    [endpoint :refer [new-endpoint]]
+    [handler :refer [new-handler]])
+   [example.handler :refer [authors-routes directors-routes]]
+   [example.db :refer [create-tables!]]
+   [example.middleware :refer [wrap-not-found]]
+   [ring.middleware.format :refer [wrap-restful-format]]
+   [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
    [environ.core :refer [env]]))
 
 (defn dev-system []
   (component/system-map
-   :db (new-h2-database DEFAULT-MEM-SPEC)
-   :app (component/using
-         (new-app #'app (fn [db] (create-table! (:db-spec db))))
-         [:db])
-   :web (component/using
+   :db (new-h2-database DEFAULT-MEM-SPEC create-tables!)
+   :authors (component/using
+             (new-endpoint authors-routes)
+             [:db])
+   :directors (component/using
+               (new-endpoint directors-routes)
+               [:db]) 
+   :handler (component/using
+             (new-handler {:middleware [[wrap-not-found :not-found]
+                                        [wrap-restful-format]
+                                        [wrap-defaults :defaults]]
+                           :defaults api-defaults
+                           :not-found  "<h2>The requested page does not exist.</h2>"})
+             [:authors :directors])
+   :http (component/using
          (new-web-server (Integer. (env :http-port)))
-         {:handler :app})))
+         [:handler])))
 
 (defn prod-system []
   (component/system-map
    :db (new-h2-database DEFAULT-DB-SPEC)
-   :app (component/using
-         (new-app #'app (fn [db] (create-table! (:db-spec db))))
-         [:db])
-   :web (component/using
+   :authors (component/using
+             (new-endpoint authors-routes)
+             [:db])
+   :directors (component/using
+               (new-endpoint directors-routes)
+               [:db]) 
+   :handler (component/using
+             (new-handler {:middleware [[wrap-not-found :not-found]
+                                        [wrap-restful-format]
+                                        [wrap-defaults :defaults]]
+                           :defaults api-defaults
+                           :not-found  "<h2>The requested page does not exist.</h2>"})
+             [:authors :directors])
+   :http (component/using
          (new-web-server (Integer. (env :http-port)))
-         {:handler :app})))
+         [:handler])))
 
